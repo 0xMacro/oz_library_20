e41cb94fa06aa40df6c910de360968bff95814b4
Gilbert <gilbertbgarza@gmail.com>
2023-06-28 16:22:45 -0500

    Sample repo

---
+++ src/Bar.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { PaymentSplitter } from "openzeppelin-contracts/finance/PaymentSplitter.sol";
+
+contract BarSplitter is PaymentSplitter {
+  constructor(address[] memory payees, uint256[] memory shares_)
+    PaymentSplitter(payees, shares_)
+  {}
+
+  /**
+   * @notice Allow eth transfers to this contract
+   */
+  fallback() external payable {}
+}
+
---
+++ src/Foo.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {BarSplitter} from "#/Bar.sol";
+
+abstract contract Foo is BarSplitter {}
